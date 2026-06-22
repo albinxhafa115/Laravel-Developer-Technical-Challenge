3b52b44407b379794434092eae6530ba99ee6883
albinxhafa115 <127633893+albinxhafa115@users.noreply.github.com>
2026-06-22 20:14:12 +0200

    Add migrations and models with relationships

---
+++ app/Models/Issue.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Issue extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['project_id', 'title', 'description', 'status', 'priority', 'due_date'];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'issue_tag');
+    }
+}
